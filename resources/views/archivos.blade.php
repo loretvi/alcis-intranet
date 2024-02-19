f0059d89008d21cb525fb3934661448cdb04d0eb
JYSC0 <jeyami2000@gmail.com>
2024-02-19 10:42:04 -0600

    archivos

--- resources/views/archivos.blade.php
+++ resources/views/archivos.blade.php
@@ -1,4 +1,66 @@
 @extends('layouts.app')
 @section('content')
+<section class="content">
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h4 class="card-title">Añadir Archivo</h4>
+                    <button type="button" class="btn btn-primary">Crear</button>
+                    <button type="button" class="btn btn-primary">Subir</button>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card text-center">
+                            <div class="card-body">
+                                <h4 class="card-title">Selecciona los permisos:</h4>
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Selecciona el permiso:
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li><a class="dropdown-item" href="#">Permiso nivel 1</a></li>
+                                        <li><a class="dropdown-item" href="#">Permiso nivel 2</a></li>
+                                        <li><a class="dropdown-item" href="#">Permiso nivel 3</a></li>
+                                        <li><a class="dropdown-item" href="#">Permiso nivel 4</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h4 class="card-title">Fecha de expiración:</h4>
+                    <div class="reservation-box">
+                    <div class="top">
+                        <div class="static">
+                        <div class="input-container" id="date-picker-container">
+                            <label for="date-from">check-in</label>
+                            <input type="date" id="date-checkin" class="date-field" name"date-from">
+                        </div>
+                        </div>
+                        <div class="flex">
+                        <div class="input-container" id="date-picker-container">
+                            <label for="date-from">check-out</label>
+                            <input type="date" id="date-checkout" class="date-field" name"">
+                        </div>
+                        
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
 
 @endsection
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
   <!-- CSS Files -->
   <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
@@ -153,6 +154,7 @@
 
     });
   </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
